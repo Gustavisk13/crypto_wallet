a0564f0e2f82fd87e510b44b503675846a662345
gustavisk <gustavolzss12@gmail.com>
2022-11-05 08:33:51 -0300

    env removed

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: "env");
   RootBinding().dependencies();
   runApp(const CryptoWalletApp());
 }
